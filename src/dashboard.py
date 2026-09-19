6b910ae31d1bc3c15ed028868d45a4b3f0ef51b9
Mahnaz Ghassemi <mahnaz.ghssm@gmail.com>
2026-09-19 15:49:24 +0330

    Add Streamlit dashboard

---
+++ src/dashboard.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from password_generator import PinGeneratorPassword, RandomPasswordGenerator, MemorablePasswordGenerator
+
+st.image("src/images/banner.jpeg")
+st.title(":zap: Password Generator")
+
+option = st.radio("Select a password generator",
+                ("Random Password", "Memorable Password", "Pin Code"))
+
+if option == "Pin Code":
+    length = st.slider("Select the length of the Pin Code", 4, 32)
+    generator = PinGeneratorPassword(length)
+elif option == "Random Password":
+    length = st.slider("Select the length of the password", 8, 100)
+    include_number = st.toggle("Include Numbers")
+    include_symbol = st.toggle("Include symbols")
+    generator = RandomPasswordGenerator(length, include_number, include_symbol)
+elif option == "Memorable Password":
+    number_of_words = st.slider("Select number of words:", 2, 10)
+    separator = st.text_input("Separator", value = "_")
+    capitalization = st.toggle("Capitalization")
+    generator = MemorablePasswordGenerator(number_of_words, separator, capitalization)
+
+password = generator.generator()
+st.write(f"Your password is : ``` {password}``` ")
